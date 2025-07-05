765bdc6d518ced06adbf619153237e5e3685b87a
limon4638 <limon.mia4182@gmail.com>
2025-07-06 01:02:44 +0600

    Delete Priyansh/frezen.js

--- Priyansh/frezen.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-const freezePath = path.join(__dirname, "..", "frozen.json");
-
-if (fs.existsSync(freezePath)) {
-  const { frozen } = JSON.parse(fs.readFileSync(freezePath));
-  if (frozen && event.body && !event.body.startsWith(".unfreeze")) return;
-}
